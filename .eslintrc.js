1ba9698d8d86dee8c7dff5f561130d72c61fcb12
AGoblinKing <AGoblinKing@isekai.dev>
2019-03-18 13:14:46 -0700

    change daround name a bunch

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
         "CODE_VERSION": "readonly"
     },
     "parserOptions": {
